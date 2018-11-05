b58f442340715c27aa8f3b5b91d9b855f62331fd
Daniel Schwartz <schw@rtz.se>
2018-11-05 17:05:18 +0100

    db tool for oracle

--- dbtool/dbtool.sh
+++ dbtool/dbtool.sh
@@ -1,1 +1,7 @@
-java -cp "dbtool-0.0.1-SNAPSHOT-jar-with-dependencies.jar;dbDriverJar.jar" se.rtz.DbTool "$@"
+
+#export JAVA_HOME="/cygdrive/c/Program Files/Java/jdk1.8.0_131"
+#export PATH=$JAVA_HOME/bin:$PATH
+
+java -cp "C:/cygwin64/home/TEMPDASCH/bin/dbtool-0.0.1-SNAPSHOT-jar-with-dependencies.jar;C:/cygwin64/home/TEMPDASCH/bin/ojdbc7-12.1.0.1.jar" se.rtz.DbTool "$@"
+
+
