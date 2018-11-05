341827c3c7ad1612fcdd015689f3b1e75202d45b
Daniel Schwartz <schw@rtz.se>
2018-11-05 17:04:30 +0100

    fixed build error

--- csvtool/csvtool.sh
+++ csvtool/csvtool.sh
@@ -1,5 +1,6 @@
 #export JAVA_HOME=....
 #export PATH=$JAVA_HOME/bin:$PATH
 
-java -cp "csvtool-0.0.3-SNAPSHOT-jar-with-dependencies.jar" se.rtz.csvtool.CsvTool "$@"
+java -jar C:/cygwin64/home/TEMPDASCH/bin/csvtool-0.0.3-SNAPSHOT-jar-with-dependencies.jar "$@"
+#java -cp "/home/TEMPDASCH/bin/csvtool-0.0.3-SNAPSHOT-jar-with-dependencies.jar" se.rtz.csvtool.CsvTool "$@"
 
